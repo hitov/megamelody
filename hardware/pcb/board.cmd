fd0b1745f517fc005283cc26edc9d936f11fb6d2
Angel Hitov <angelhitov@gmail.com>
2012-06-23 17:23:02 +0300

    [HW] generate board.pcb from schematics
    Signed-off-by: Angel Hitov <angelhitov@gmail.com>

---
+++ hardware/pcb/board.cmd
@@ -0,0 +1,125 @@
+# Pin name action command file
+
+# Start of element D1
+ChangePinName(D1, 2, 2)
+ChangePinName(D1, 1, 1)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element J3
+ChangePinName(J3, 1, 1)
+ChangePinName(J3, 2, 2)
+
+# Start of element C10
+ChangePinName(C10, 2, 2)
+ChangePinName(C10, 1, 1)
+
+# Start of element C9
+ChangePinName(C9, 2, 2)
+ChangePinName(C9, 1, 1)
+
+# Start of element C8
+ChangePinName(C8, 2, -)
+ChangePinName(C8, 1, +)
+
+# Start of element C7
+ChangePinName(C7, 2, -)
+ChangePinName(C7, 1, +)
+
+# Start of element J2
+ChangePinName(J2, 2, 2)
+ChangePinName(J2, 1, 1)
+
+# Start of element U2
+ChangePinName(U2, 3, 3)
+ChangePinName(U2, 2, 2)
+ChangePinName(U2, 1, 1)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element SPK1
+ChangePinName(SPK1, 2, 2)
+ChangePinName(SPK1, 1, 1)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element T1
+ChangePinName(T1, G, G)
+ChangePinName(T1, S, S)
+ChangePinName(T1, D, D)
+
+# Start of element C6
+ChangePinName(C6, 2, 2)
+ChangePinName(C6, 1, 1)
+
+# Start of element C5
+ChangePinName(C5, 2, 2)
+ChangePinName(C5, 1, 1)
+
+# Start of element C4
+ChangePinName(C4, 2, 2)
+ChangePinName(C4, 1, 1)
+
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
+
+# Start of element C2
+ChangePinName(C2, 2, 2)
+ChangePinName(C2, 1, 1)
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
+
+# Start of element Q1
+ChangePinName(Q1, 2, 2)
+ChangePinName(Q1, 1, 1)
+
+# Start of element J1
+ChangePinName(J1, 6, 6)
+ChangePinName(J1, 5, 5)
+ChangePinName(J1, 4, 4)
+ChangePinName(J1, 3, 3)
+ChangePinName(J1, 2, 2)
+ChangePinName(J1, 1, 1)
+
+# Start of element U1
+ChangePinName(U1, unknown, unknown)
+ChangePinName(U1, unknown, unknown)
+ChangePinName(U1, unknown, unknown)
+ChangePinName(U1, unknown, unknown)
+ChangePinName(U1, unknown, unknown)
+ChangePinName(U1, 27, "(ADC4/SDA) PC4")
+ChangePinName(U1, 20, AREF)
+ChangePinName(U1, 18, AVCC)
+ChangePinName(U1, 28, "(ADC5/SCL) PC5")
+ChangePinName(U1, 27, "(ADC4/SDA) PC4")
+ChangePinName(U1, 26, "(ADC3) PC3")
+ChangePinName(U1, 25, "(ADC2) PC2")
+ChangePinName(U1, 24, "(ADC1) PC1")
+ChangePinName(U1, 23, "(ADC0) PC0")
+ChangePinName(U1, 19, ADC6)
+ChangePinName(U1, 17, "PB5 (SCK)")
+ChangePinName(U1, 16, "PB4 (MISO)")
+ChangePinName(U1, 15, "PB3 (MOSI/OC2)")
+ChangePinName(U1, 14, "PB2 (SS/OC1B)")
+ChangePinName(U1, 13, "PB1 (OC1A)")
+ChangePinName(U1, 12, "PB0 (ICP1)")
+ChangePinName(U1, 11, "PD7 (AIN1)")
+ChangePinName(U1, 10, "PD6 (AIN0)")
+ChangePinName(U1, 9, "PD5 (T1)")
+ChangePinName(U1, 8, "PB7 (XTAL2/TOSC2)")
+ChangePinName(U1, 7, "PB6 (XTAL1/OSC1)")
+ChangePinName(U1, 2, "PD4 (XCK/T0)")
+ChangePinName(U1, 1, "PD3 (INT1)")
+ChangePinName(U1, 32, "PD2 (INT0)")
+ChangePinName(U1, 31, "PD1 (TxD)")
+ChangePinName(U1, 30, "PD0 (RxD)")
+ChangePinName(U1, 29, "(RESET) PC6")
